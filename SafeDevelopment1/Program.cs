6d36cce5704d67ff486a0ba625fe0af3c90651b6
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-08-24 15:59:46 +0300

    Добавьте файлы проекта.

---
+++ SafeDevelopment1/Program.cs
@@ -0,0 +1,24 @@
+using NLog.Web;
+
+namespace CardStorageService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+
+                }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+    }
+}
---
+++ SafeDevelopment1/Startup.cs
@@ -0,0 +1,93 @@
+using CardStorageService.Data;
+using CardStorageService.Services;
+using CardStorageService.Services.Impl;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardStorageService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            #region Configure Options Services
+
+            services.Configure<DatabaseOptions>(options =>
+            {
+                Configuration.GetSection("Settings:DatabaseOptions").Bind(options);
+            });
+
+            #endregion
+
+            services.AddScoped<IClientRepositoryService, ClientRepository>();
+            services.AddScoped<ICardRepositoryService, CardRepository>();
+
+            services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+                logging.RequestBodyLogLimit = 4096;
+                logging.ResponseBodyLogLimit = 4096;
+                logging.RequestHeaders.Add("Authorization");
+                logging.RequestHeaders.Add("X-Real-IP");
+                logging.RequestHeaders.Add("X-Forwarded-For");
+            });
+
+            #region Configure EF DBContext Service (CardStorageService Database)
+
+            services.AddDbContext<CardStorageServiceDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["Settings:DatabaseOptions:ConnectionString"]);
+            });
+
+            #endregion
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CardStorageService", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CardStorageService v1"));
+            }
+
+            app.UseRouting();
+            app.UseHttpLogging();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
