b2879d35dc4d8fbbc03591881500df5d9d7d9b47
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-09-06 17:40:43 +0300

    AuthenticateController added

---
+++ SafeDevelopment1/Controllers/AuthenticateController.cs
@@ -0,0 +1,33 @@
+﻿using CardService.Models.Requests;
+using CardService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardService.Controllers
+{
+    [Authorize]
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IAuthenticateService _authenticateService;
+        public AuthenticateController(IAuthenticateService authenticateService)
+        {
+            _authenticateService = authenticateService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("login")]
+        [ProducesResponseType(typeof(AuthenticationResponse), StatusCodes.Status200OK)]
+        public IActionResult Login([FromBody] AuthenticationRequest authenticationRequest)
+        {
+            AuthenticationResponse authenticationResponse = _authenticateService.Login(authenticationRequest);
+            if (authenticationResponse.Status == Models.AuthenticationStatus.Success)
+            {
+                Response.Headers.Add("X-Session-Token", authenticationResponse.SessionInfo.SessionToken);
+            }
+            return Ok(authenticationResponse);
+        }
+    }
+}
